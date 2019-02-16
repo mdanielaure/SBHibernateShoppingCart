9e6d241c13bdd344d7babc4a56b64ef898f5a560
JPCOLONNA\DLMATCHIM <dl-matchim@jpcolonna.fr>
2019-02-16 18:27:47 +0100

    first commit

---
+++ src/test/java/com/danie/sbhibernateshoppingcart/SbHibernateShoppingCartApplicationTests.java
@@ -0,0 +1,17 @@
+package com.danie.sbhibernateshoppingcart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbHibernateShoppingCartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
